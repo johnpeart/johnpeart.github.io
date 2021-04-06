8c249bc6316a2c7c57708073b2be8aec4421fcdb
johnpeart <johnpeart@users.noreply.github.com>
2021-04-06 22:35:09 +0000

    deploy: d49400c31d9a3ada0b1fa44b2a8fb8514268e136

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,7 +1,14 @@
 function toggleMenu() {
-  var menu = document.getElementById("header-menu-links");
+  var menu = document.getElementById("header-menu-toggle");
   menu.classList.toggle("open");
+  menu.classList.toggle("closed");
   
-  var menu = document.getElementById("header-menu-toggle-image");
+  var menu = document.getElementById("header-menu-items");
   menu.classList.toggle("open");
+  menu.classList.toggle("closed");
+  if (menu.classList.contains("open")) {
+    menu.setAttribute('aria-expanded', 'true')
+  } else {
+    menu.setAttribute('aria-expanded', 'false')
+  }
 }
